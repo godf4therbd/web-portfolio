dddc188e571b0be5831c9cb18fb1fa24085ea0ac
godf4therbd <65154816+godf4therbd@users.noreply.github.com>
2024-05-09 17:22:03 +0600

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,12 @@
+// JavaScript sdocument.addEventListener("DOMContentLoaded", function() {
+// Get the button and experience section elements
+const experienceBtn = document.getElementById("experience-btn");
+const experienceSection = document.getElementById("experience-section");
+
+// Add click event listener to the button
+experienceBtn.addEventListener("click", function () {
+    // Scroll to the experience section smoothly
+    experienceSection.scrollIntoView({ behavior: "smooth" });
+});
+});
+ource code
